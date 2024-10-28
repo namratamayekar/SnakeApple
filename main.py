ec5585038f0aeaeb4b254f799cff2f10a71aee20
nams1710 <nams1710@gmail.com>
2024-10-28 17:53:05 +0100

    Added Game over Logic

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 import random
 
 SIZE = 40
-
+background_color = (21, 92, 21)
 
 class Apple:
     def __init__(self, parent_screen):
@@ -37,7 +37,7 @@ class Snake:
         self.y.append(-1)
 
     def draw(self):
-        self.parent_screen.fill((21, 92, 21)) #to remove previous block after block movement
+        self.parent_screen.fill((background_color)) #to remove previous block after block movement
         for i in range(self.length):
             self.parent_screen.blit(self.block,(self.x[i], self.y[i]))
         pygame.display.flip()
@@ -76,7 +76,7 @@ class Game:
     def __init__(self):
         pygame.init()
         self.surface = pygame.display.set_mode((1000,800)) # size: 1000,800
-        self.surface.fill((21, 92, 21)) # fill the surface wth green color(RGB) values
+        self.surface.fill((background_color)) # fill the surface wth green color(RGB) values
         self.snake = Snake(self.surface, 1)
         self.snake.draw()
         self.apple = Apple(self.surface)
@@ -94,10 +94,30 @@ class Game:
         self.display_scores()
         pygame.display.flip()
 
+        #snake colliding with apple
         if self.is_collision(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
             self.snake.increase_length()
             self.apple.move()
     
+        #snake colliding with itself
+        for i in range(3, self.snake.length):
+            if self.is_collision(self.snake.x[0], self.snake.y[0], self.snake.x[i],self.snake.y[i]):
+                raise "Game Over"
+                      
+    def show_game_over(self):
+        self.surface.fill(background_color)
+        font = pygame.font.SysFont('arial', 30)
+        line1 = font.render(f"Game is Over ! Your score is: {self.snake.length}", True, (255,255,255))
+        self.surface.blit(line1, (200, 300))
+        time.sleep(1)
+        line2 = font.render("To play again press Enter. To Exit press Escape !", True, (255,255,255))
+        self.surface.blit(line2, (200, 350))
+        pygame.display.flip()
+    
+    def reset(self):
+        self.snake = Snake(self.surface, 1)
+        self.apple = Apple(self.surface)
+
     def display_scores(self):
         font = pygame.font.SysFont('arial', 30)
         score = font.render(f"Score: {self.snake.length}", True, (255,255,255))
@@ -107,26 +127,38 @@ class Game:
 
     def run(self):
         running = True
+        pause = False
 
         while running:
             for event in pygame.event.get():
                 if event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
                         running = False
-                    if event.key == K_UP:
-                        self.snake.move_up()
-                    if event.key == K_DOWN:
-                        self.snake.move_down()
-                    if event.key == K_LEFT:
-                        self.snake.move_left()
-                    if event.key == K_RIGHT:
-                        self.snake.move_right()
+                    if event.key == K_RETURN:
+                        pause = False
+                    if not pause:
+                        if event.key == K_UP:
+                            self.snake.move_up()
+                        if event.key == K_DOWN:
+                            self.snake.move_down()
+                        if event.key == K_LEFT:
+                            self.snake.move_left()
+                        if event.key == K_RIGHT:
+                            self.snake.move_right()
                 
                 elif event.type == QUIT:
                     running = False
+            
+            try:
+                if not pause:
+                    self.play()
+            except Exception as e:
+                self.show_game_over()
+                pause = True
+                self.reset()
 
-            self.play()
-            time.sleep(0.4)
+
+            time.sleep(0.2)
 
 
 if __name__ == "__main__":
